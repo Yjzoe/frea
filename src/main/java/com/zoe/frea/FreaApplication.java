a49adc681fbd551ca01b9c388692477d56ccd4ca
yj <anuefn@naverc.om>
2020-05-14 16:52:21 +0900

    create

---
+++ src/main/java/com/zoe/frea/FreaApplication.java
@@ -0,0 +1,13 @@
+package com.zoe.frea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreaApplication.class, args);
+    }
+
+}
