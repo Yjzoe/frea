67963010fe97fd4f4f185d55510b67403b89bc66
yj <anuefn@naverc.om>
2020-05-20 20:14:53 +0900

    chapter4

--- src/main/java/com/zoe/frea/service/posts/PostsService.java
+++ src/main/java/com/zoe/frea/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.zoe.frea.service.posts;
 
 import com.zoe.frea.domain.posts.Posts;
 import com.zoe.frea.domain.posts.PostsRepository;
+import com.zoe.frea.web.dto.PostsListResponseDto;
 import com.zoe.frea.web.dto.PostsResponseDto;
 import com.zoe.frea.web.dto.PostsSaveRequestDto;
 import com.zoe.frea.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,18 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
